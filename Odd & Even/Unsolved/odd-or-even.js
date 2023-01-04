45836ba6717e38572ef5d199df3e7609d4b7d88b
Brian King <bnk5532@gmail.com>
2023-01-04 13:16:46 -0500

    Odd or Even tests added and tested my code successfully against tests

---
+++ Odd & Even/Unsolved/odd-or-even.js
@@ -0,0 +1,21 @@
+// Write code to return the string "odd" if `num` is an odd number
+// and return the string "even" if `num` is an even number
+
+var oddOrEven = function(num) {
+  if(num % 2)
+  return "odd"
+  else return "even"
+  
+};
+
+// my code works above. 
+
+// alternative correct option below
+
+// var oddOrEven = function(num) {
+//   if (num % 2 === 0) {
+//     return "even";
+//   } else {
+//     return "odd";
+//   }
+// };
