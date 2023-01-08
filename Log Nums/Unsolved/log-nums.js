d6506c94c0f6a2e39fe8c9d74a787dde8b0cd901
Brian King <bnk5532@gmail.com>
2023-01-08 15:24:14 -0500

    pseudo added to solution of log-nums

--- Log Nums/Unsolved/log-nums.js
+++ Log Nums/Unsolved/log-nums.js
@@ -1,6 +1,14 @@
 // Write code to print all numbers from 1 to `num`
 // Assume `num` will be a positive number
 
+
+//define logNums variable
+//write function for num
+//use loop from index 1 as it is starting point
+//run loop until num
+//iterate by one loop for each index
+//log value of index at each pass
+
 var logNums = function(num) {
     for (var i = 1; i <= num; i++)
         console.log(i)
