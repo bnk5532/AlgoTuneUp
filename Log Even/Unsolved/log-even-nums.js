79a162809543c7098038db3eee110c0e33185bbe
Brian King <bnk5532@gmail.com>
2023-01-08 15:21:08 -0500

    pseudo code added

--- Log Even/Unsolved/log-even-nums.js
+++ Log Even/Unsolved/log-even-nums.js
@@ -1,3 +1,10 @@
 // Write code to print all even numbers from 0 to `num`
 // Assume `num` will be a positive number
 
+//define variable for logEvenNums
+//write function for num
+//loop from 0 start by 1 num.
+//end loop at last num.
+//iterate at one whole num for each loop or by two since starting from zero and only logging even.
+//use if to check even
+//console.log results
