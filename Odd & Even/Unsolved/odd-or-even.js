68794c94bac006a40ad9d9e3106f9ea3e52fda84
Brian King <bnk5532@gmail.com>
2023-01-08 15:26:56 -0500

    pseudo added to odd/even practice

--- Odd & Even/Unsolved/odd-or-even.js
+++ Odd & Even/Unsolved/odd-or-even.js
@@ -1,6 +1,11 @@
 // Write code to return the string "odd" if `num` is an odd number
 // and return the string "even" if `num` is an even number
 
+//define variable for oddOrEven
+//write function for num
+//use if for finding even numbers
+//return odd or even respectively
+
 var oddOrEven = function(num) {
   if(num % 2)
   return "odd"
