3ea38e04e7daa6a2cf1aa7a9113e3aba1fdbb689
Brian King <bnk5532@gmail.com>
2023-01-09 17:25:44 -0500

    pseudo added for countdown

--- Countdown/Unsolved/countdown.js
+++ Countdown/Unsolved/countdown.js
@@ -1,4 +1,10 @@
 // Write code to print all numbers from `num` down to 1
 // You may assume `num` will be a positive number
 
+//define countDown variable
+//write function for countDown
+//pass countDown function num arg
+//counting down from num, so loop with downward iteration (--)
+//console.log(countDown) to print
+
 
