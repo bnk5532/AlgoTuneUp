51e8ccffce7e18e42f71bf0b1e0c3658e08bace0
Brian King <bnk5532@gmail.com>
2023-01-09 17:46:52 -0500

    pseudo for fizz-buzz added

--- Fizz Buzz/Unsolved/fizz-buzz.js
+++ Fizz Buzz/Unsolved/fizz-buzz.js
@@ -3,3 +3,10 @@
 // If a number is evenly divisible by 5 print "Buzz"
 // If a number is evenly divisible by both 3 AND 5, print "Fizz Buzz"
 // If a number is not divisible by 3 or 5, print the number
+
+//define variable for fizzBuzz
+//write function for fizzBuss
+//pass function arg of arr for random array of numbers coming in
+//loop over the length of the given array
+//write if/else statement for 3 scenarios
+//print fizz, buzz or fizz buzz respectively else print number
