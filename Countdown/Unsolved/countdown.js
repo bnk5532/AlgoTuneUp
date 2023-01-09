14bc3a36252fc1886ce3914f38c229e09155a6e6
Brian King <bnk5532@gmail.com>
2023-01-09 17:30:36 -0500

    solved countdown again test

--- Countdown/Unsolved/countdown.js
+++ Countdown/Unsolved/countdown.js
@@ -1,10 +1,15 @@
 // Write code to print all numbers from `num` down to 1
 // You may assume `num` will be a positive number
 
-//define countDown variable
+//define countdown variable
 //write function for countDown
-//pass countDown function num arg
+//pass countdown function num arg
 //counting down from num, so loop with downward iteration (--)
-//console.log(countDown) to print
+//console.log(i) to print
 
+var countdown = function(num){
+    for (i = num; i > 0; i--) {
+        console.log(i);  
+    }
+}
 
