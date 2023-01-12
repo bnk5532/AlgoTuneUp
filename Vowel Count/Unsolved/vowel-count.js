87ba7fd90a680f57a37736fb281c2ed76b89660e
Brian King <bnk5532@gmail.com>
2023-01-11 21:07:03 -0500

    vount count for string code working against test

--- Vowel Count/Unsolved/vowel-count.js
+++ Vowel Count/Unsolved/vowel-count.js
@@ -5,3 +5,19 @@
 //define empty array for results
 //define vowels variable
 //loop over string and total vowels
+
+var vowelCount = function(str){
+
+var result = 0
+var vowels = ["a", "e", "i", "o", "u"]
+
+for (let i = 0; i < str.length; i++) {
+    var letter = str[i].toLowerCase();
+    //searches string for vowel match, if not matched, -1 otherwise +1 to result
+    if (vowels.indexOf(letter) !== -1) {
+        result +=1;
+    }
+    console.log(result)
+}
+return result;
+}
