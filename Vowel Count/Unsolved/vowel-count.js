8e6c541b57f3aa1cd025db87614216bf4a21dabe
Brian King <bnk5532@gmail.com>
2023-01-11 20:57:31 -0500

    pseode code added

--- Vowel Count/Unsolved/vowel-count.js
+++ Vowel Count/Unsolved/vowel-count.js
@@ -1,2 +1,7 @@
 // Write code to return the the number of vowels in `str`
 
+//define vowelCount variable
+//write function that takes in a given string
+//define empty array for results
+//define vowels variable
+//loop over string and total vowels
