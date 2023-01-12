03c7a7ece18295d59a5a58c8cdcc1b24b40348d6
Brian King <bnk5532@gmail.com>
2023-01-11 20:37:14 -0500

    pseudo code added

--- Max Num/Unsolved/max-num.js
+++ Max Num/Unsolved/max-num.js
@@ -1,2 +1,7 @@
 // Write code to return the largest number in the given array
 
+//define maxNum variable
+//write function that takes in an array
+//loop over length of array
+//write condition for maxNum => currentNum, etc.
+//variable for result should greatest number in given array
