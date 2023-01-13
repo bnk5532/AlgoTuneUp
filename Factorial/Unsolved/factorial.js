7af39ac0ab2d239fa58ee5fd869862a8d504a679
Brian King <bnk5532@gmail.com>
2023-01-13 18:20:29 -0500

    function working against test.

--- Factorial/Unsolved/factorial.js
+++ Factorial/Unsolved/factorial.js
@@ -5,3 +5,11 @@
 //loop DOWN from/through given num. (if "4", then should loop down from 4 * 3 * 2 * 1, which will result in 24.)
 //return result after loop completed, integers totalled in result variable.
 
+var factorial = function (num) {
+  var result = 1;
+
+  for (i = num; i > 1; i--) {
+    result = result * i;
+  }
+  return result;
+};
