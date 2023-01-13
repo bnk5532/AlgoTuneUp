01f6846aeb47c9eff78af934463b5914c545e691
Brian King <bnk5532@gmail.com>
2023-01-12 19:45:52 -0500

    pseudo for palindrome added

--- Palindrome/Unsolved/is-palindrome.js
+++ Palindrome/Unsolved/is-palindrome.js
@@ -1,3 +1,8 @@
 // Write code that returns true if `str` is a palindrome, and false if `str` is not a palindrome
 
+//define variable for isPalinedrome
+//write function that takes in a string
+//needs variable defined for string in reverse, so it can be compared to original string
+//if/else to return true or false
+
 
