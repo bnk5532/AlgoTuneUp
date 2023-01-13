23844e37da7a196bf75de768e782616c5347b3a9
Brian King <bnk5532@gmail.com>
2023-01-12 19:22:15 -0500

    reworked test, function and html file to work for minNum

--- Min Num /Unsolved/min-num.js
+++ Min Num /Unsolved/min-num.js
@@ -1,2 +1,23 @@
 // Write code to return the largest number in the given array
 
+//define minNum variable
+//write function that takes in an array
+//loop over length of array
+//write condition for minNum => currentNum, etc.
+//variable for result should lowest number in given array
+
+var minNum = function(arr) {
+  var min = arr[0]
+
+  for (i = 0; i < arr.length; i++) {
+    const currentNum = arr[i];
+    
+    if (currentNum < min) {
+      min = currentNum;
+      console.log(min);
+      //only prints number if lower than previous numbers due to currentNum > max comparison
+    }
+  }
+  return min;
+  
+}
