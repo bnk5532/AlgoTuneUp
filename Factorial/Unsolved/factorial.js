f07c9316ec817de696534a17171a748a48f64ba7
Brian King <bnk5532@gmail.com>
2023-01-13 18:10:50 -0500

    pseudo added for factorial

--- Factorial/Unsolved/factorial.js
+++ Factorial/Unsolved/factorial.js
@@ -1,2 +1,7 @@
 // Write code to create a function that returns the factorial of `num`
 
+//define variable for factorial
+//write function that takes in arg of num
+//loop DOWN from/through given num. (if "4", then should loop down from 4 * 3 * 2 * 1, which will result in 24.)
+//return result after loop completed, integers totalled in result variable.
+
