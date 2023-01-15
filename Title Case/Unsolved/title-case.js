b5ed5d4e1d2ff2133e808ad66a977c4beac62480
Brian King <bnk5532@gmail.com>
2023-01-14 20:40:37 -0500

    pseudo for title case added

--- Title Case/Unsolved/title-case.js
+++ Title Case/Unsolved/title-case.js
@@ -1,3 +1,9 @@
 // Write code to create a function takes a string and returns a new string with the first letters of each word capitalized
 
-
+//define variable for titleCase
+//write function to take in a given string
+//use split method to break up string into individual letters
+//need to define variable that identifies a word
+//use upperCase method to uppercase only first letter of word at index 0 of that word
+//need to join word into new array for final result.
+//return final result.
