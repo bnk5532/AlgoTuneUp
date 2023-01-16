dddb62eacbfdf74ec9263033826cceee1102badd
Brian King <bnk5532@gmail.com>
2023-01-15 21:01:52 -0500

    title case problem solved and working against test

--- Title Case/Unsolved/title-case.js
+++ Title Case/Unsolved/title-case.js
@@ -7,3 +7,23 @@
 //use upperCase method to uppercase only first letter of word at index 0 of that word
 //need to join word into new array for final result.
 //return final result.
+
+var titleCase = function(str){
+    //new array for new string after capitalization
+    var result = [];
+    //split method to separate words in array - space in line 15 required or it will split into individual letters
+    var words = str.split(' ');
+    console.log(words)
+
+    for (let i = 0; i < words.length; i++) {
+        //split method here has no space, b/c now individual words are separated, the individual letters are separated.
+        var word = words[i].split('');
+        //after word split to individual letters in a word, index 0 is capitalized.
+        word[0] = word[0].toUpperCase();
+        console.log(word)
+        //reverse splits with joins, no space, then single spaced to join letters into words, then words into complete string.
+        result.push(word.join(''));
+        
+    }
+    return result.join(' ');
+}
