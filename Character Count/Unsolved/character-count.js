d4dcd288ab5c81afb5289be1889cf4b5f43d0628
Brian King <bnk5532@gmail.com>
2023-01-15 21:10:10 -0500

    pseudo for charCount problem added

--- Character Count/Unsolved/character-count.js
+++ Character Count/Unsolved/character-count.js
@@ -1,3 +1,9 @@
 // Write code to create a function that accepts a string and returns an object containing the number of times each character appears in the string
 
 
+//define variable for characterCount
+//write function that takes in a string
+//use charMap method to create a key for the letter and count
+//loop over the length of the string
+//use if/else conditional to iterate over the letter count
+//return object for charMap after loop complete
