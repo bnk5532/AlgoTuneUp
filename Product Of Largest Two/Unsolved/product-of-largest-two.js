aafd02cfb880bcce7aad6e8f965a897d9b33a43a
Brian King <bnk5532@gmail.com>
2023-01-17 20:09:20 -0500

    pseudo added for product of largest two

--- Product Of Largest Two/Unsolved/product-of-largest-two.js
+++ Product Of Largest Two/Unsolved/product-of-largest-two.js
@@ -1,3 +1,8 @@
 // Write code to create a function that accepts an array of numbers, finds the largest two numbers, and returns the product of the two
 
-
+//define variable for productOfLargestTwo 
+//write function to accept array
+//loop over length of array using similar if statement as max-num problem
+//or sort array largest to smallest, then select index 0 and 1.
+//write function for largestNum * secondLargest
+//return product of index 1 & 2.
