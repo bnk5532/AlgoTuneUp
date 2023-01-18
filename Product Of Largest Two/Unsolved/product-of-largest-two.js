8ee221ecd6657592bfa3f054a40ca847e1013b39
Brian King <bnk5532@gmail.com>
2023-01-17 20:39:47 -0500

    product of largest two solved, works again test - comments updated

--- Product Of Largest Two/Unsolved/product-of-largest-two.js
+++ Product Of Largest Two/Unsolved/product-of-largest-two.js
@@ -1,8 +1,28 @@
 // Write code to create a function that accepts an array of numbers, finds the largest two numbers, and returns the product of the two
 
-//define variable for productOfLargestTwo 
+//define variable for productOfLargestTwo
 //write function to accept array
+//define variables for largest and secondLargest.
 //loop over length of array using similar if statement as max-num problem
 //or sort array largest to smallest, then select index 0 and 1.
 //write function for largestNum * secondLargest
 //return product of index 1 & 2.
+
+var productOfLargestTwo = function (arr) {
+  var largest = null;
+  var secondLargest = null;
+
+  for (let i = 0; i < arr.length; i++) {
+    var currentNum = arr[i];
+
+    if (currentNum > largest || largest === null) {
+      secondLargest = largest;
+      largest = currentNum;
+      console.log(secondLargest, largest)
+      //used else if to add new condition if first condition is false. Null object is falsy for both local variables.
+    } else if (currentNum > secondLargest || secondLargest === null) {
+      secondLargest = currentNum;
+    }
+  }
+  return largest * secondLargest;
+};
