8ddf870e2ce64c2a19d2105c688c2490f1c823ce
Brian Nicholas King <bnk@Brians-MacBook-Pro.local>
2023-01-31 21:43:02 -0500

    draw tree printed correctly, but probably needs refactoring

--- Draw Tree/draw-tree.js
+++ Draw Tree/draw-tree.js
@@ -4,3 +4,15 @@
 // ***
 // ****
 // *****
+
+//define variable for drawTree
+//write function to loop over index 5x and add a "*" with each pass
+//console.log("*")
+
+var stars = ["*", "**","***","****","*****"]
+
+var drawTree = (arr)=>{
+    for (let i = 0; i < arr.length; i++) {
+    }
+}
+console.log(stars)
